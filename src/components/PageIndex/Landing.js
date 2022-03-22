0f6a839b7e721449b25b2dd437ee78d24ac4fa24
Erick Escobar <erickescobar2500@gmail.com>
2022-03-22 15:35:11 -0600

    my

---
+++ src/components/PageIndex/Landing.js
@@ -0,0 +1,10 @@
+import React from "react";
+//import mancha from "../../img/mancha1.png"
+import './Header.css'
+export default function PageIndex() {
+    return(
+        <div>
+            <label>hola</label>
+        </div>
+    )
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,13 @@
 
 import  React from "react"
 import Header from "./header/Header.js";
-import Landing from "./Landing/Landng.js";
+import PageIndex from "./PageIndex/PageIndex.js";
 
 export default function Components(){
     return(
         <div>
         <Header></Header>
-        <Landing></Landing>
+        <PageIndex></PageIndex>
         </div>
     );
 }
